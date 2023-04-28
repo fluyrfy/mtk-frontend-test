ac2969e504203ff6b0774c98c91314380679c460
Frank Liao <frank880703@gmail.com>
2023-04-28 15:16:13 +0800

    feat: project done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "../styles/globals.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
